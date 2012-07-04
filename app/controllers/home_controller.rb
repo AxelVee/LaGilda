5873550f376a51df26079c16a8adfecebba40afa
Axel_Vee <axel_vee@hotmail.it>
2012-07-04 11:57:57 +0200

    Everything up

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,36 @@
+class HomeController < ApplicationController
+
+
+  def termsofuse
+    @title = "Termini di servizio"  
+  end
+  
+  def home
+    
+  end
+  
+  def contact
+    @title = "Contact us"
+  end
+  
+  def about
+    @title = "About"
+  end
+
+  def news
+    
+  end
+
+  def users
+  end
+
+  def posts
+  end
+  
+  def homepage
+    
+  end
+
+  
+ 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CastingUP::Application.config.secret_token = 'b3764104f4befa367ee144f42962c9e9e3a1371012790278a8da4e71e9b14e69ee7ee6c791184265b03d90dc31decad0ab4b191f68faafbe26ad4720ac616542'
